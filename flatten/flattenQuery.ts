e1b8386aee0ff99f152795a0976ff3f66eaf6478
SDBagel <sdbagel@gmail.com>
2021-12-22 23:41:24 -0800

    Fix flattenQuery function call

--- flatten/flattenQuery.ts
+++ flatten/flattenQuery.ts
@@ -3,9 +3,9 @@ import {
   Flattened,
 	PaginationResult
 } from "../schemas/mod.ts";
-import { flattenProperty } from "./mod.ts";
+import { flattenPage } from "./mod.ts";
 
 // Flattens a database query result
 export function flattenQuery(res: PaginationResult): Flattened[] {
-  return res.results.map((page) => flattenProperty(page));
+  return res.results.map((page) => flattenPage(page));
 }
