48d1210933d4b9fe9dcfbd4da9da4bd937f37470
SDBagel <sdbagel@gmail.com>
2022-01-05 19:14:55 -0800

    Fix flattenDatabase properties not being included

--- flatten/flattenDatabase.ts
+++ flatten/flattenDatabase.ts
@@ -14,9 +14,6 @@ export function flattenDatabase(db: Flattenable): Flattened {
     created_time: db.created_time,
     last_edited_time: db.last_edited_time
   };
-  // Flatten each individual property recursively
-  for (const key of Object.keys(db.properties))
-    flat.properties[key] = flattenProperty(db.properties[key]);
   // Set properties to their type directly
   for (const key of Object.keys(flat.properties))
     flat.properties[key] = flat.properties[key].type;
